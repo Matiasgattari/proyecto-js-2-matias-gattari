fee46a894d5e45f205ae22c8171831e25f1349ca
daravhel <lea.mg90@gmail.com>
2022-07-22 01:39:02 -0300

    2do commit trato de crear carrito

--- carrito.js
+++ carrito.js
@@ -1,4 +1,6 @@
-import { listaProductos } from "./productos.js";
+import {
+    listaProductos
+} from "./productos.js";
 
 // Renderizado de los productos en la pagina 
 
@@ -8,16 +10,34 @@ for (const producto of listaProductos) {
     let carritoCompras = document.createElement("div");
     carritoCompras.innerHTML = `
     <div class="card m-5 ${producto.nombre} margenTarjetas ">
-        <div class="card-body contenedorTarjetas align-items-center justify-content-center d-flex flex-column">
-             <h3 class="card-title m-2 nombreTarjeta">Cerveza ${producto.nombre}</h3>
-             <p class="card-text m-2 nombreTarjeta">Precio $ ${producto.precio}</p>
+        <div class="card-body contenedorTarjetas align-items-center justify-content-center d-flex flex-column divTarjeta">
+             <h3 class="card-title m-2 nombreTarjeta nombre">Cerveza ${producto.nombre}</h3>
+             <p class="card-text m-2 nombreTarjeta precio">Precio $ ${producto.precio}</p>
+             <p class="card-text m-2 nombreTarjeta codigo">${producto.codigo}</p>
              <input type="number" placeholder="Cantidad" class="m-2">
-             <button id="botonCarrito" type="submit" class="btn btn-primary mt-5"> Agregar al carrito</button>
+             <button type="submit" class="btn btn-primary mt-5 botonCarrito  ${producto.codigo}"> Agregar al carrito</button>
         </div>
     </div>`;
     contenedor.append(carritoCompras);
- }
+};
+// console.log(contenedor)
 
 
 
-// const divContenedorCarrito = 
+
+
+
+
+const arrayCarrito = [];
+
+const botonCarrito = document.querySelectorAll(".botonCarrito");
+
+function agregarAlCarrito() {
+   
+console.log("producto agregado al carrito")
+};
+
+botonCarrito.forEach((botonCarrito) => {
+    botonCarrito.addEventListener("click", agregarAlCarrito);
+    
+});
